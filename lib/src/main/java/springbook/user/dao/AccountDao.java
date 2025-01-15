897eba782d6f92d425888b6aeb338c5561a378b3
DKn1102 <kjg3595@naver.com>
2025-01-15 14:37:31 +0900

    1.4 제어의 역전

---
+++ lib/src/main/java/springbook/user/dao/AccountDao.java
@@ -0,0 +1,78 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class AccountDao {
+	//1.3 DAO의 확장
+//	private SimpleConnectionMaker simpleConnectionMaker;
+	
+	//1.3 DAO의 확장 (인터페이스)
+	private ConnectionMaker connectionMaker;
+	
+	public AccountDao(ConnectionMaker connectionMaker) {
+//		simpleConnectionMaker = new SimpleConnectionMaker();
+		this.connectionMaker = connectionMaker;
+	}
+	
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("org.mariadb.jdbc.Driver");
+		//1.2 DAO의 분리
+//		Connection c = getConnection();
+		
+		//1.3 DAO의 확장
+		Connection c = connectionMaker.makeConnection();
+		
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+		
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("org.mariadb.jdbc.Driver");
+		//1.2 DAO의 분리
+//		Connection c = getConnection();
+		
+		//1.3 DAO의 확장
+		Connection c = connectionMaker.makeConnection();
+		
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+		
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+	
+	//1.1 초난감 DAO
+//	private Connection getConnection() throws ClassNotFoundException, SQLException {
+//		Class.forName("org.mariadb.jdbc.Driver");
+//		Connection c = DriverManager.getConnection("jdbc:mariadb://localhost:3306/board", "root", "1234");
+//		
+//		return c;
+//	}
+	
+	//1.2 DAO의 분리
+//	public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+}
--- lib/src/main/java/springbook/user/test/UserDaoTest.java
+++ lib/src/main/java/springbook/user/test/UserDaoTest.java
@@ -2,23 +2,27 @@ package springbook.user.test;
 
 import java.sql.SQLException;
 
-import springbook.user.dao.ConnectionMaker;
-import springbook.user.dao.NConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//		ConnectionMaker connectionMaker = new DConnectionMaker();
+		//1.3 DAO의 확장
+		//		ConnectionMaker connectionMaker = new DConnectionMaker();
 		
-		ConnectionMaker connectionMaker = new NConnectionMaker();
-		UserDao dao = new UserDao(connectionMaker);
+		//1.3 DAO의 확장
+//		ConnectionMaker connectionMaker = new NConnectionMaker();
+//		UserDao dao = new UserDao(connectionMaker);
+		
+		//1.4 제어의 역전
+		UserDao dao = new DaoFactory().userDao();
 		
 		User user = new User();
-		user.setId("david4");
-		user.setName("강정구4");
-		user.setPassword("Y4");
+		user.setId("david5");
+		user.setName("강정구5");
+		user.setPassword("Y5");
 		
 		dao.add(user);
 		
